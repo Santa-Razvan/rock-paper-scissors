52ca23bfa8e0af53ac275c3d68b0e34b4a1b1f3d
Santa-Razvan <126788093+Santa-Razvan@users.noreplay.github.com>
2023-03-28 17:30:13 +0300

    implemented a user prompt that asks how many rounds to play and a points system, project done

--- index.js
+++ index.js
@@ -5,32 +5,77 @@ function getComputerChoice() {
     return choices[randomNumber];
 }
 
-let playerSelection = "";
+function getPlayerChoice() {
+    let playerSelection = " ";
+    do {
+        playerSelection = prompt("Pick something between rock, paper, scrissors !:").toLowerCase();
 
-do {
-    playerSelection = prompt("Pick something between rock, paper, scrissors !:").toLowerCase();
-} while (!((playerSelection === 'rock') ||
-    (playerSelection === 'paper') ||
-    (playerSelection === 'scrissors')));
+    } while (!((playerSelection === 'rock') ||
+        (playerSelection === 'paper') ||
+        (playerSelection === 'scrissors')));
+    return playerSelection;
+}
+let computerSelection;
+let playerSelection;
 
-let computerSelection = getComputerChoice();
+let playerCount = 0;
+let computerCount = 0;
+// let computerSelection = getComputerChoice();
+// let playerSelection = getPlayerChoice();
 
 function play(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return console.log("It's a tie you both picked " + playerSelection + "!");
+
     } else if ((playerSelection === 'rock') && (computerSelection === 'scrissors')) {
+        playerCount++;
         return console.log("You win! Rock beats Scrissors!");
+
     } else if ((playerSelection === 'rock') && (computerSelection === 'paper')) {
+        computerCount++;
         return console.log("You lose! Paper beats Rock!")
+
     } else if ((playerSelection === 'paper') && (computerSelection === 'scrissors')) {
+        computerCount++;
         return console.log("You lose! Scrissors beats Paper");
+
     } else if ((playerSelection === 'paper') && (computerSelection === 'rock')) {
+        playerCount++;
         return console.log("You win! Paper beats Rock");
+
     } else if ((playerSelection === 'scrissors') && (computerSelection === 'rock')) {
+        computerCount++;
         return console.log("You lose! Rock beats Scrissors!")
-    } else return console.log("You win! Scrissors beats Paper!");
+
+    } else {
+        playerCount++;
+        return console.log("You win! Scrissors beats Paper!");
+    }
 }
 
-console.log(playerSelection);
-console.log(computerSelection);
-play(playerSelection, computerSelection);
+const i = prompt("How many games do you wanna play?");
+let counter = 0;
+
+function game() {
+    while (counter < i) {
+        playerSelection = getPlayerChoice();
+        computerSelection = getComputerChoice();
+
+        console.log(playerSelection);
+        console.log(computerSelection);
+
+        play(playerSelection, computerSelection);
+        counter++;
+    }
+    if (playerCount === computerCount) {
+        return console.log("\nIt's a tie!");
+
+    } else if (playerCount < computerCount) {
+        return console.log("\nThe computer won!");
+    } else return console.log("\nYou won!");
+}
+
+//console.log(playerSelection);
+//console.log(computerSelection);
+// play(playerSelection, computerSelection);
+game();
