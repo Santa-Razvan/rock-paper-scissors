ef58131fe62b74852ab0dc9b2c02d8c6ac04eb83
Santa-Razvan <126788093+Santa-Razvan@users.noreplay.github.com>
2023-04-03 20:13:21 +0300

    completed the UI, it's functional in each case, need to add a stop once someone reaches 5 points

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const body = document.querySelector('body');
 const container = document.createElement('div');
 const paraResult = document.createElement('p');
 const paraYourScore = document.createElement('p');
+const paraComputerScore = document.createElement('p');
 
 let computerSelection;
 let playerSelection;
@@ -35,11 +36,19 @@ body.style.cssText = "display:flex; justify-content:center; align-items:center; 
 container.style.cssText = "height:250px; width:500px; background-color: rgb(20,50,100); margin-top:20px; text-align:center;";
 paraResult.style.cssText = "height:30px; margin-top:50px; color:white;";
 
+paraYourScore.textContent = "You: " + 0;
+paraYourScore.style.cssText = "text-align:left; padding-left:50px; font-size:30px; margin-top:50px; color:white;"
+
+paraComputerScore.textContent = "Computer: " + 0;
+paraComputerScore.style.cssText = "text-align:right; padding-right:50px; font-size:30px; margin-top:-62px;color:white;";
+
 body.appendChild(rockButton);
 body.appendChild(paperButton);
 body.appendChild(scrissorsButton);
 body.appendChild(container);
 container.appendChild(paraResult);
+container.appendChild(paraYourScore);
+container.appendChild(paraComputerScore);
 
 //function to get a random choice
 function getComputerChoice() {
@@ -71,26 +80,32 @@ function play(playerSelection, computerSelection) {
 
     } else if ((playerSelection === 'rock') && (computerSelection === 'scrissors')) {
         playerCount++;
+        paraYourScore.textContent = "You: " + +playerCount;
         return paraResult.textContent = "You win! Rock beats Scrissors!";
 
     } else if ((playerSelection === 'rock') && (computerSelection === 'paper')) {
         computerCount++;
+        paraComputerScore.textContent = "Computer: " + +computerCount;
         return paraResult.textContent = "You lose! Paper beats Rock!";
 
     } else if ((playerSelection === 'paper') && (computerSelection === 'scrissors')) {
         computerCount++;
+        paraComputerScore.textContent = "Computer: " + +computerCount;
         return paraResult.textContent = "You lose! Scrissors beats Paper";
 
     } else if ((playerSelection === 'paper') && (computerSelection === 'rock')) {
         playerCount++;
+        paraYourScore.textContent = "You: " + +playerCount;
         return paraResult.textContent = "You win! Paper beats Rock";
 
     } else if ((playerSelection === 'scrissors') && (computerSelection === 'rock')) {
         computerCount++;
+        paraComputerScore.textContent = "Computer: " + +computerCount;
         return paraResult.textContent = "You lose! Rock beats Scrissors!";
 
     } else {
         playerCount++;
+        paraYourScore.textContent = "You: " + +playerCount;
         return paraResult.textContent = "You win! Scrissors beats Paper!";
     }
 }
@@ -105,24 +120,24 @@ function play(playerSelection, computerSelection) {
 // let counter = 0;
 
 //this function runs for more games at a time, based on the number given by the user
-function game() {
-    while (counter < i) {
-        playerSelection = getPlayerChoice();
-        computerSelection = getComputerChoice();
+// function game() {
+//     while (counter < i) {
+//         playerSelection = getPlayerChoice();
+//         computerSelection = getComputerChoice();
 
-        console.log(playerSelection);
-        console.log(computerSelection);
+//         console.log(playerSelection);
+//         console.log(computerSelection);
 
-        play(playerSelection, computerSelection);
-        counter++;
-    }
-    if (playerCount === computerCount) {
-        return console.log("\nIt's a tie!");
+//         play(playerSelection, computerSelection);
+//         counter++;
+//     }
+//     if (playerCount === computerCount) {
+//         return console.log("\nIt's a tie!");
 
-    } else if (playerCount < computerCount) {
-        return console.log("\nThe computer won!");
-    } else return console.log("\nYou won!");
-}
+//     } else if (playerCount < computerCount) {
+//         return console.log("\nThe computer won!");
+//     } else return console.log("\nYou won!");
+// }
 
 //console.log(playerSelection);
 //console.log(computerSelection);
